457c732dd51b91c1ecd0b64800d006aac3e78eab
isabelahh <00001114286291SP@al.educacao.sp.gov.br>
2024-05-29 13:45:01 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+createCanvas(600, 600);
+background("black");
+}
+
+function draw() {
+stroke("purple");
+fill("pink");
+
+// console.log(mouseIsPressed)
+
+if (mouseIsPressed) {
+  rect(mouseX, mouseY, 20, 35);
+}
+}
